b55e4ba0ae142a7da045c9b17595cb6a51b71e97
Tiffany Poss <tiffanyposs@.com>
2018-11-14 14:56:54 -0500

    setup eslint and basic webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
 
-// testing
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/, // do not apply babel to node_modules
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/,
+      },
+    ],
+  },
 };
